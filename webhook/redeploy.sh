26b29aa6f77b7e66053861c41c764eb11b5f29c1
LeoC-II <alex.beauchamp.1@ens.etsmtl.ca>
2025-09-10 12:08:42 -0400

    Ajustement docker-compose.yml

--- webhook/redeploy.sh
+++ webhook/redeploy.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-touch file.txt
+cd "/home/log430/log430-a25-labo0"
+
+echo "Get updates from git"
+git fetch --all
+git pull
+
+echo "Build & deploy new image"
+docker compose up --build --remove-orphans
+docker system prune -af --volumes
