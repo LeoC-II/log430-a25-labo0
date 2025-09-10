1c7e849e3862b9ebde4f4b5b3d5e1589a93f38e7
LeoC-II <alex.beauchamp.1@ens.etsmtl.ca>
2025-09-10 09:54:08 -0400

    Ajout du rapport et des tests

---
+++ docs/rapport.md
@@ -0,0 +1,8 @@
+# Rapport laboratoire 0
+
+## Question 1: Échec d'un test
+Lors de l'échec d'un test, les détails de l'erreur sont affichés, incluant la différence entre la réponse attendue
+et la réponse ici.
+![Screenshot de l'échec](echec-pytest.jpg)
+
+
--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -6,8 +6,23 @@ Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 
 from calculator import Calculator
 
-def test_app():
+class TestCalculator():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    def test_app(self):
+        assert TestCalculator.my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+    # TODO: ajoutez les tests
+    def test_addition(self):
+        assert TestCalculator.my_calculator.addition(2, 3) == (2+3)
+
+    def test_subtraction(self):
+        assert TestCalculator.my_calculator.subtraction(2, 3) == (2-3)
+
+    def test_multiplication(self):
+        assert TestCalculator.my_calculator.multiplication(2, 3) == (2*3)
+
+    def test_division(self):
+        assert TestCalculator.my_calculator.division(2, 3) == (2/3)
+
+    def test_division_by_zero(self):
+        assert TestCalculator.my_calculator.division(2, 0) == "Erreur : division par zéro"
