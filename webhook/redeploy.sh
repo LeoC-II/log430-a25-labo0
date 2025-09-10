c2de3fcd090709449760cc9f003c41f699e95188
LeoC-II <alex.beauchamp.1@ens.etsmtl.ca>
2025-09-10 12:45:06 -0400

    Version finale de redeploy.sh

--- webhook/redeploy.sh
+++ webhook/redeploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-cd "/home/log430/log430-a25-labo0"
+#cd "/home/log430/log430-a25-labo0"
 
 echo "Get updates from git"
 git fetch --all
 git pull
 
 echo "Build & deploy new image"
-docker compose up --build --remove-orphans
+docker stop $(docker ps -q --filter ancestor=labo0-calculator:latest)
+docker build -t labo0-calculator .
+docker run -it labo0-calculator
 docker system prune -af --volumes
