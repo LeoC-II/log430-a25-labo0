27b354f5214bd9ae79508a03c777099411593faa
LeoC-II <alex.beauchamp.1@ens.etsmtl.ca>
2025-09-10 12:47:35 -0400

    Version finale 2 de redeploy.sh

--- webhook/redeploy.sh
+++ webhook/redeploy.sh
@@ -7,6 +7,6 @@ git pull
 
 echo "Build & deploy new image"
 docker stop $(docker ps -q --filter ancestor=labo0-calculator:latest)
+docker system prune -af --volumes
 docker build -t labo0-calculator .
 docker run -it labo0-calculator
-docker system prune -af --volumes
